26a7ee5758b627e0511f9d8cad2ea6f91163ec8c
Vickouma77 <Vickprestone20@gmail.com>
2024-05-03 16:24:08 +0300

    routes

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import flash, redirect, render_template, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+   user = {'username': 'Prestone'}
+   posts = [
+      {
+         'author': 'James',
+         'body': 'Always huffing and puffing'
+      },
+      {
+         'author': 'Linzie',
+         'body': 'Growing metals and irons'
+      },
+      {
+         'author': 'Mainoo',
+         'body': 'Ole on the wheel'
+      }
+   ]
+
+   return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   if form.validate_on_submit():
+      flash('Login requested for user {}, remember_me={}'.format(
+         form.username.data, form.remember_me.data))
+      return redirect(url_for('index'))
+   return render_template('login.html', title='sign in', form=form)
