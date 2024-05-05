da411545052a362f1ddc390cbbb436936bf984ba
Vickouma77 <Vickprestone20@gmail.com>
2024-05-05 18:37:04 +0300

    adding avatar method to User class

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ import sqlalchemy.orm as so
 from app import db
 from app import login
 from flask_login import UserMixin
+from hashlib import md5
 
 
 @login.user_loader
@@ -28,6 +29,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
