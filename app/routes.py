6530c59857236803b796f56b6218fa38614b0a90
Vickouma77 <Vickprestone20@gmail.com>
2024-05-06 12:05:20 +0300

    profile view function

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,11 @@ from flask import(
    url_for
 )
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import(
+    LoginForm, 
+    RegistrationForm, 
+    EditProfileForm
+    )
 from flask_login import(
    current_user, 
    login_user, 
@@ -91,3 +95,18 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('You cahnges have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='edit profile', form=form)
