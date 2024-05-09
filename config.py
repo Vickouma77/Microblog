e2503b7397df354b12f05fc20ca239d9609aef8a
Vickouma77 <Vickprestone20@gmail.com>
2024-05-09 14:00:11 +0300

    post per page configuration

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['myemail@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
