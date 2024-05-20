3e893efb147572d8d1e88d3b11520a2647bddd57
Vickouma77 <Vickprestone20@gmail.com>
2024-05-20 12:02:05 +0300

    Initialize Flask-babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +8,10 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
