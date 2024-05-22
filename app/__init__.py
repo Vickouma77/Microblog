9045ef4c7e9b38e1e42b2f5db3d52bb32a03a54f
Vickouma77 <Vickprestone20@gmail.com>
2024-05-22 09:49:19 +0300

    Marking Texts to Translate In Python Source Code

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,11 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 
 def get_locale():
     request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +20,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
@@ -50,6 +52,7 @@ if not os.path.exists('logs'):
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
 
 
 from app import routes, models, errors
